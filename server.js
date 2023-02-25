d039f2eea381b2805c9c319ae5ec0c5a24ff3aff
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-02-25 17:01:17 -0600

    Added basic functionality to delete a note

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const notes = require('./db/db.json');
+const uuid = require('./helpers/uuid.js');
 
 const PORT = process.env.PORT || 3001;
 
@@ -28,6 +29,7 @@ app.post('/api/notes', (req, res) => {
 
     if ( title && text ) {
         const newNote = {
+            id: uuid(),
             title,
             text
         };
@@ -52,6 +54,32 @@ app.post('/api/notes', (req, res) => {
     }
 });
 
+app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const target = notes.find( (note) => note.id == id );
+
+    if (target) {
+        const deleted = notes.splice(notes.indexOf(target), 1);
+        const notesString = JSON.stringify(notes, null, 4);
+        fs.writeFile('./db/db.json', notesString, (err) => {
+            err ? 
+                console.error(err)
+                : console.log(`Note with title "${req.body.title}" has been saved`);
+            }
+        );
+
+        const response = {
+            status: 'success',
+            body: deleted
+        }
+
+        console.log(response);
+        res.status(200).json(response);
+    } else {
+        res.status(404).send("The requested note did not exist in the database");
+    }
+    
+})
 
 
 
