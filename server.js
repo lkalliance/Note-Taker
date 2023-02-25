7b33d91ce7913bd8a739cb39acfb209225150e17
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-02-25 13:25:58 -0600

    Programmed server to save submitted note to the JSON file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const notes = require('./db/db.json');
 
 const PORT = process.env.PORT || 3001;
 
@@ -20,7 +21,17 @@ app.get('/notes', (req, res) => {
 
 app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'db/db.json'));
-})
+});
+
+app.post('/api/notes', (req, res) => {
+    notes.push(req.body);
+    const newNotes = JSON.stringify(notes, null, 4);
+    fs.writeFile('./db/db.json', newNotes, (err) => {
+        err ? console.error(err) : console.log(
+            `Note with title "${req.body.title}" has been saved`
+        );
+    });
+});
 
 
 
