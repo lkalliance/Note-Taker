193f3096ca5abcb694c57179dfcc3502d53dddfb
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-02-27 10:48:09 -0600

    Put routes to api calls in a separate module

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use('/api/notes', require('./routes/api.js'))
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -19,70 +21,6 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
-
-app.get('/api/notes', (req, res) => {
-    res.send(notes);
-});
-
-app.post('/api/notes', (req, res) => {
-    const { title, text } = req.body;
-
-    if ( title && text ) {
-        const newNote = {
-            id: req.body.id || uuid(),
-            title,
-            text
-        };
-
-        notes.push(newNote);
-        const notesString = JSON.stringify(notes, null, 4);
-        fs.writeFile('./db/db.json', notesString, (err) => {
-            err ? 
-                console.error(err)
-                : console.log(`Note with title "${req.body.title}" has been saved`);
-            }
-        );
-        const response = {
-            status: 'success',
-            body: newNote
-        }
-
-        console.log(response);
-        res.status(201).json(response);
-    } else {
-        res.status(500).json('Error in posting review')
-    }
-});
-
-app.delete('/api/notes/:id', (req, res) => {
-    const id = req.params.id;
-    const target = notes.find( (note) => note.id == id );
-
-    if (target) {
-        const deleted = notes.splice(notes.indexOf(target), 1);
-        const notesString = JSON.stringify(notes, null, 4);
-        fs.writeFile('./db/db.json', notesString, (err) => {
-            err ? 
-                console.error(err)
-                : console.log(`Note with title "${req.body.title}" has been saved`);
-            }
-        );
-
-        const response = {
-            status: 'success',
-            body: deleted
-        }
-
-        console.log(response);
-        res.status(200).json(response);
-    } else {
-        res.status(404).send("The requested note did not exist in the database");
-    }
-    
-})
-
-
-
 
 
 app.listen(PORT, () => {
