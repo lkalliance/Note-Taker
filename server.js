30f8a2d0c1ec533dda1acaf3229c6e9a41eaf34c
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-03-01 11:20:28 -0600

    switched routing to utilize wild card catch-all

--- server.js
+++ server.js
@@ -17,12 +17,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // Routes to serve HTML pages
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-});
-
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // Start the server
